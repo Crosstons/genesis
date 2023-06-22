526f036d645ce127c5af259e3bf0cffa9c50235f
vking45 <d.vraj.4523@gmail.com>
2023-06-23 00:35:25 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './views/Landing';
 import Main from './views/Main';
 import Minting from './Main/Minting';
+import Dashboard from './Main/Dashboard';
 
 function App() {
   return (
     <div className="font-pop">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/minting' element={<Minting />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
       <Minting />
     </div>
   );
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Sidebar from '../Main/Sidebar'
-import Dashboard from '../Main/Dashboard'
-import Minting from '../Main/Minting'
+
 function Main() {
   return (
     <div>
