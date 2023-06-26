54377be84adcc99377b4c195f6037b6c9b1632e0
vking45 <d.vraj.4523@gmail.com>
2023-06-26 23:54:14 +0530

    added new collections

--- src/Main/Utils.jsx
+++ src/Main/Utils.jsx
@@ -77,4 +77,35 @@ export const nfts = [
       location: "Andes Mountain (South America)",
       metadata: "ipfs://bafyreiasmtkvqj7a5ktc3qmotzkevkf2srkdfucaxemmemolswoxddgabm/metadata.json"   
     },
+    { 
+      id: 4,
+      address :"0x28755f5f26606694F4b3857B89743547AB86f1d2",
+      name: 'FKT',
+      collection: 'Franklin Tree', 
+      image: 'https://cdn.discordapp.com/attachments/1063991835871154277/1122024320202448926/e0ce5f27-c560-4d8f-8608-5fa351c86489.jpg',
+      description: "The Franklin Tree, named after Benjamin Franklin, is a rare and endangered deciduous tree native to the southeastern United States. It bears beautiful white flowers in spring and boasts a unique leaf shape. Due to habitat loss and overharvesting, the Franklin Tree is now critically endangered in the wild. Conservation initiatives focus on preserving the remaining populations and raising awareness about its ecological importance. Efforts are underway to ensure the survival of this iconic tree for future generations to enjoy.",
+      location: "Washington (USA)",
+      metadata: "ipfs://bafyreibjwpfcj7z4vdcl4e2jyezf44w6uydlmstgk5cmzam5xvd3ofjeay/metadata.json"   
+    },
+    { 
+      id: 5,
+      address :"0xE705E8c366144b2689c37B523b5cA0aBaf95Efaf",
+      name: 'CFT',
+      collection: 'Coffin Tree', 
+      image: 'https://cdn.discordapp.com/attachments/1063991835871154277/1122019992003674202/ae6617f9-d4ba-4b31-9a54-2ce10bb8b504.jpg',
+      description: "The Coffin Tree, also known as the Tetrameles nudiflora, is a remarkable tree species found in Southeast Asia. It is named for its peculiar resemblance to a wooden coffin when its branches are bare. The Coffin Tree holds cultural and religious significance in some regions, where it is believed to be associated with ancestral spirits. Conservation efforts are crucial to protect this unique tree and its ecological value. By raising awareness and implementing sustainable practices, we can help ensure the survival and preservation of the Coffin Tree for future generations to appreciate its distinct beauty and cultural significance.",
+      location: "Taiwan",
+      metadata: "ipfs://bafyreibmkqe4jco33zkik7jolemcr27o7warx263jibiuyvxfsm4llbaeq/metadata.json"   
+    },
+    { 
+      id: 6,
+      // update address
+      address :"0xE705E8c366144b2689c37B523b5cA0aBaf95Efaf",
+      name: 'BFF',
+      collection: 'Brodiaea filifolia', 
+      image: 'https://cdn.discordapp.com/attachments/1063991835871154277/1122019549496233984/1e7d6cdd-dee8-48ae-b7f8-3bd478bd6444.jpg',
+      description: "Brodiaea filifolia, also known as Thread-Leaved Brodiaea, is a stunning flowering plant native to California. With its delicate leaves and vibrant purple flowers, it adds beauty to its surroundings. Sadly, it is threatened by habitat loss and urban development, risking its existence. Conservation efforts are vital to protect this species and preserve California's biodiversity. Let's raise awareness and promote habitat conservation to safeguard the Brodiaea filifolia and maintain the ecological balance.",
+      location: "California (USA)",
+      metadata: "ipfs://bafyreifimhtmqnr5kjtyiagpgrb2kaazfsaqiev5nee6utg7gbafp4a27a/metadata.json"   
+    },
   ];
